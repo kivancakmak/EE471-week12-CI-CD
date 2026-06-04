5af8c07096823e305bcae4317cc5db1e7ae160e9
kivancakmak <kivanccakmak35@gmail.com>
2026-06-04 14:57:00 +0300

    style: apply ruff formatting

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,5 +24,5 @@ admin.site.index_title = "Welcome to Polls Administration"
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
